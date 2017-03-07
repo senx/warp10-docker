7904f4c0325dfc0096aae3602879493bd3ae83cd
David Morin <morin.david.bzh@gmail.com>
2017-03-08 00:24:45 +0100

    a specific data directory can be specified to Warp10 and Sensision init by now

--- Dockerfile
+++ Dockerfile
@@ -7,10 +7,12 @@ RUN apk update && apk add bash curl python
 # Installing build-dependencies
 RUN apk add --virtual=build-dependencies ca-certificates wget
 
-ENV WARP10_VERSION=1.0.15-1-ga07cb94
-ENV WARP10_URL=https://bintray.com/artifact/download/cityzendata/generic/io/warp10/warp10/$WARP10_VERSION
+ENV WARP10_VERSION=1.2.5-7-g8fdcc9e
+#ENV WARP10_URL=https://bintray.com/artifact/download/cityzendata/generic/io/warp10/warp10/$WARP10_VERSION
+
 # FOR local docker build (dev)
 # ENV WARP10_URL=http://{local_ip}:{localport}
+ENV WARP10_URL=http://192.168.1.138:8000
 
 # Getting warp10
 RUN mkdir /opt \
@@ -20,10 +22,11 @@ RUN mkdir /opt \
   && rm warp10-$WARP10_VERSION.tar.gz \
   && ln -s  /opt/warp10-$WARP10_VERSION /opt/warp10
 
-ENV SENSISION_VERSION=1.0.9
-ENV SENSISION_URL=https://dl.bintray.com/cityzendata/generic/io/warp10/sensision-service/$SENSISION_VERSION
+ENV SENSISION_VERSION=1.0.12-2-g80861c2
+#ENV SENSISION_URL=https://dl.bintray.com/cityzendata/generic/io/warp10/sensision-service/$SENSISION_VERSION
 # FOR local docker build (dev)
 # ENV SENSISION_URL = http://{local_ip}:{localport}
+ENV SENSISION_URL=http://192.168.1.138:8000
 
 # Getting Sensision
 RUN cd /opt \
@@ -35,34 +38,23 @@ RUN cd /opt \
 # Deleting build-dependencies
 RUN apk del build-dependencies
 
-ENV QUANTUM_VERSION=2.1.25
-ENV QUANTUM_URL=https://dl.bintray.com/cityzendata/generic/io/warp10/quantum/$QUANTUM_VERSION
-# Getting quantum
-RUN mkdir /opt/quantum-$QUANTUM_VERSION \
-    && cd /opt/quantum-$QUANTUM_VERSION \
-    && curl $QUANTUM_URL/quantum-$QUANTUM_VERSION.tar.gz -o quantum-$QUANTUM_VERSION.tar.gz \
-    && tar xzf quantum-$QUANTUM_VERSION.tar.gz \
-    && rm quantum-$QUANTUM_VERSION.tar.gz \
-    && ln -s /opt/quantum-$QUANTUM_VERSION /opt/quantum
-
-
 ENV JAVA_HOME=/usr \
   WARP10_HOME=/opt/warp10-${WARP10_VERSION} SENSISION_HOME=/opt/sensision-${SENSISION_VERSION} \
-  WARP10_VOLUME=/data MAX_LONG=3153600000000
+  WARP10_VOLUME=/data MAX_LONG=3153600000000 \
+  WARP10_DATA_DIR=/data/warp10
 
 ENV WARP10_JAR=${WARP10_HOME}/bin/warp10-${WARP10_VERSION}.jar \
   WARP10_CONF=${WARP10_HOME}/etc/conf-standalone.conf
 
 # REPLACE hard link in configuration template with symbolic link
 RUN sed -i 's/^standalone\.home.*/standalone\.home = \/opt\/warp10/' ${WARP10_HOME}/templates/conf-standalone.template
-RUN sed -i 's/^sensision\.home.*/sensision\.home = \/opt\/sensision\/data/' ${SENSISION_HOME}/templates/sensision.template
+RUN sed -i 's/^sensision\.home.*/sensision\.home = \/opt\/sensision/' ${SENSISION_HOME}/templates/sensision.template
 RUN sed -i 's/^sensision\.scriptrunner\.root.*/sensision\.scriptrunner\.root= \/opt\/sensision\/scripts/' ${SENSISION_HOME}/templates/sensision.template
 
 # REPLACE hard link in log4j.properties with symbolic link
 RUN sed -i "s/\/opt\/warp10-${WARP10_VERSION}/\/opt\/warp10/" ${WARP10_HOME}/etc/log4j.properties
 
 COPY warp10.start.sh ${WARP10_HOME}/bin/warp10.start.sh
-COPY worf.sh ${WARP10_HOME}/bin/worf.sh
 COPY setup.sh ${WARP10_HOME}/bin/setup.sh
 RUN chmod +x ${WARP10_HOME}/bin/*.sh
 
--- setup.sh
+++ setup.sh
@@ -8,34 +8,41 @@ else
   useradd -d ${SENSISION_HOME} -M -r sensision
 fi
 
-# WARP10 install
+# WARP10 - install and manage upgrade
 if [ ! -d ${WARP10_VOLUME}/warp10 ]
 then
   echo "Install Warp10"
-  ${WARP10_HOME}/bin/warp10-standalone.bootstrap
-  mkdir -p ${WARP10_VOLUME}/warp10
-  mv ${WARP10_HOME}/etc ${WARP10_VOLUME}/warp10/etc
-  ln -s ${WARP10_VOLUME}/warp10/etc ${WARP10_HOME}/etc
-  mv ${WARP10_HOME}/data ${WARP10_VOLUME}/warp10/data
-  ln -s ${WARP10_VOLUME}/warp10/data ${WARP10_HOME}/data
-  mv ${WARP10_HOME}/macros ${WARP10_VOLUME}/warp10/macros
-  ln -s ${WARP10_VOLUME}/warp10/macros ${WARP10_HOME}/macros
-  mv ${WARP10_HOME}/jars ${WARP10_VOLUME}/warp10/jars
-  ln -s ${WARP10_VOLUME}/warp10/jars ${WARP10_HOME}/jars
-  mv ${WARP10_HOME}/warpscripts ${WARP10_VOLUME}/warp10/warpscripts
-  ln -s ${WARP10_VOLUME}/warp10/warpscripts ${WARP10_HOME}/warpscripts
-  mv ${WARP10_HOME}/logs ${WARP10_VOLUME}/warp10/logs
-  ln -s ${WARP10_VOLUME}/warp10/logs ${WARP10_HOME}/logs
+  # Stop/start to init config
+  ${WARP10_HOME}/bin/warp10-standalone.init start && ${WARP10_HOME}/bin/warp10-standalone.init stop
 else
   echo "Warp10 already installed"
-  #clean
-  rm -rf ${WARP10_HOME}/etc ${WARP10_HOME}/data ${WARP10_HOME}/macros ${WARP10_HOME}/warpscripts ${WARP10_HOME}/logs
-  #link Warp10
+
+  rm -rf ${WARP10_HOME}/etc
   ln -s ${WARP10_VOLUME}/warp10/etc ${WARP10_HOME}/etc
-  ln -s ${WARP10_VOLUME}/warp10/data ${WARP10_HOME}/data
+
+  rm -rf ${WARP10_HOME}/leveldb
+  ln -s ${WARP10_VOLUME}/warp10/leveldb ${WARP10_HOME}/leveldb
+
+  rm -rf ${WARP10_HOME}/macros
   ln -s ${WARP10_VOLUME}/warp10/macros ${WARP10_HOME}/macros
+  
+  rm -rf ${WARP10_HOME}/warpscripts
   ln -s ${WARP10_VOLUME}/warp10/warpscripts ${WARP10_HOME}/warpscripts
+  
+  rm -rf ${WARP10_HOME}/logs
   ln -s ${WARP10_VOLUME}/warp10/logs ${WARP10_HOME}/logs
+  
+  rm -rf ${WARP10_HOME}/jars
+  ln -s ${WARP10_VOLUME}/warp10/jars ${WARP10_HOME}/jars
+
+  rm -rf ${WARP10_HOME}/lib
+  ln -s ${WARP10_VOLUME}/warp10/lib ${WARP10_HOME}/lib
+
+  rm -rf ${WARP10_HOME}/datalog
+  ln -s ${WARP10_VOLUME}/warp10/datalog ${WARP10_HOME}/datalog
+
+  rm -rf ${WARP10_HOME}/datalog_done
+  ln -s ${WARP10_VOLUME}/warp10/datalog_done ${WARP10_HOME}/datalog_done
 fi
 
 # Sensision install
@@ -43,18 +50,16 @@ if [ ! -d ${WARP10_VOLUME}/sensision ]
 then
   echo "Install Sensision"
   ${SENSISION_HOME}/bin/sensision.bootstrap
-  mkdir -p ${WARP10_VOLUME}/sensision
+  mkdir -p ${WARP10_VOLUME}/sensision/data
   mv ${SENSISION_HOME}/etc ${WARP10_VOLUME}/sensision/etc
   ln -s ${WARP10_VOLUME}/sensision/etc ${SENSISION_HOME}/etc
-  mv ${SENSISION_HOME}/data ${WARP10_VOLUME}/sensision/data
   ln -s ${WARP10_VOLUME}/sensision/data ${SENSISION_HOME}/data
   mv ${SENSISION_HOME}/scripts ${WARP10_VOLUME}/sensision/scripts
   ln -s ${WARP10_VOLUME}/sensision/scripts ${SENSISION_HOME}/scripts
-
 else
   echo "Sensision already installed"
   #clean
-  rm -rf  ${SENSISION_HOME}/etc ${SENSISION_HOME}/data  ${SENSISION_HOME}/scripts
+  rm -rf  ${SENSISION_HOME}/etc ${SENSISION_HOME}/scripts
   # link sensision
   ln -s ${WARP10_VOLUME}/sensision/etc ${SENSISION_HOME}/etc
   ln -s ${WARP10_VOLUME}/sensision/data ${SENSISION_HOME}/data
