f0892707f16cb35932524ce867adb4d5567bb657
xavier <marin.xavier@gmail.com>
2022-06-15 16:50:51 +0200

    Quote to prevent word splitting/globbing

--- setup.sh
+++ setup.sh
@@ -18,72 +18,72 @@
 set -euo pipefail
 
 # WARP10 - install and manage upgrade
-if [ ! -d ${WARP10_DATA_DIR} ]; then
+if [ ! -d "${WARP10_DATA_DIR}" ]; then
   echo "Install Warp 10"
-  ${WARP10_HOME}/bin/warp10-standalone.init bootstrap
+  "${WARP10_HOME}/bin/warp10-standalone.init" bootstrap
 else
   echo "Warp 10 already installed"
 
-  rm -rf ${WARP10_HOME}/etc
-  ln -s ${WARP10_DATA_DIR}/etc ${WARP10_HOME}/etc
+  rm -rf "${WARP10_HOME}/etc"
+  ln -s "${WARP10_DATA_DIR}/etc" "${WARP10_HOME}/etc"
 
-  rm -rf ${WARP10_HOME}/leveldb
-  ln -s ${WARP10_DATA_DIR}/leveldb ${WARP10_HOME}/leveldb
+  rm -rf "${WARP10_HOME}/leveldb"
+  ln -s "${WARP10_DATA_DIR}/leveldb" "${WARP10_HOME}/leveldb"
 
-  rm -rf ${WARP10_HOME}/macros
-  ln -s ${WARP10_DATA_DIR}/macros ${WARP10_HOME}/macros
+  rm -rf "${WARP10_HOME}/macros"
+  ln -s "${WARP10_DATA_DIR}/macros" "${WARP10_HOME}/macros"
 
-  rm -rf ${WARP10_HOME}/warpscripts
-  ln -s ${WARP10_DATA_DIR}/warpscripts ${WARP10_HOME}/warpscripts
+  rm -rf "${WARP10_HOME}/warpscripts"
+  ln -s "${WARP10_DATA_DIR}/warpscripts" "${WARP10_HOME}/warpscripts"
 
-  rm -rf ${WARP10_HOME}/logs
-  ln -s ${WARP10_DATA_DIR}/logs ${WARP10_HOME}/logs
+  rm -rf "${WARP10_HOME}/logs"
+  ln -s "${WARP10_DATA_DIR}/logs" "${WARP10_HOME}/logs"
 
-  rm -rf ${WARP10_HOME}/jars
-  ln -s ${WARP10_DATA_DIR}/jars ${WARP10_HOME}/jars
+  rm -rf "${WARP10_HOME}/jars"
+  ln -s "${WARP10_DATA_DIR}/jars" "${WARP10_HOME}/jars"
 
-  rm -rf ${WARP10_HOME}/lib
-  ln -s ${WARP10_DATA_DIR}/lib ${WARP10_HOME}/lib
+  rm -rf "${WARP10_HOME}/lib"
+  ln -s "${WARP10_DATA_DIR}/lib" "${WARP10_HOME}/lib"
 
-  rm -rf ${WARP10_HOME}/datalog
-  ln -s ${WARP10_DATA_DIR}/datalog ${WARP10_HOME}/datalog
+  rm -rf "${WARP10_HOME}/datalog"
+  ln -s "${WARP10_DATA_DIR}/datalog" "${WARP10_HOME}/datalog"
 
-  rm -rf ${WARP10_HOME}/datalog_done
-  ln -s ${WARP10_DATA_DIR}/datalog_done ${WARP10_HOME}/datalog_done
+  rm -rf "${WARP10_HOME}/datalog_done"
+  ln -s "${WARP10_DATA_DIR}/datalog_done" "${WARP10_HOME}/datalog_done"
 
-  if [ -d ${WARP10_DATA_DIR}/hfiles ]
+  if [ -d "${WARP10_DATA_DIR}/hfiles" ]
   then
-    ln -s ${WARP10_DATA_DIR}/hfiles ${WARP10_HOME}/hfiles
+    ln -s "${WARP10_DATA_DIR}/hfiles" "${WARP10_HOME}/hfiles"
   fi
 fi
 
 # HFiles
-if [ ! -d ${WARP10_DATA_DIR}/hfiles ]
+if [ ! -d "${WARP10_DATA_DIR}/hfiles" ]
 then
   echo "Creating HFiles directory"
-  mkdir ${WARP10_DATA_DIR}/hfiles
-  ln -s ${WARP10_DATA_DIR}/hfiles ${WARP10_HOME}/hfiles
+  mkdir "${WARP10_DATA_DIR}/hfiles"
+  ln -s "${WARP10_DATA_DIR}/hfiles" "${WARP10_HOME}/hfiles"
 fi
 
 # Sensision install
-if [ ! -d ${SENSISION_DATA_DIR} ]; then
+if [ ! -d "${SENSISION_DATA_DIR}" ]; then
   echo "Install Sensision"
   # Stop/start to init config
-  ${SENSISION_HOME}/bin/sensision.init bootstrap
+  "${SENSISION_HOME}/bin/sensision.init" bootstrap
 else
   echo "Sensision already installed"
   #clean
-  rm -rf ${SENSISION_HOME}/etc ${SENSISION_HOME}/scripts ${SENSISION_HOME}/logs ${SENSISION_HOME}/metrics ${SENSISION_HOME}/targets ${SENSISION_HOME}/queued
+  rm -rf "${SENSISION_HOME}/etc" "${SENSISION_HOME}/scripts" "${SENSISION_HOME}/logs" "${SENSISION_HOME}/metrics" "${SENSISION_HOME}/targets" "${SENSISION_HOME}/queued"
   # link sensision
-  ln -s ${SENSISION_DATA_DIR}/etc ${SENSISION_HOME}/etc
-  ln -s ${SENSISION_DATA_DIR}/scripts ${SENSISION_HOME}/scripts
-  ln -s ${SENSISION_DATA_DIR}/logs ${SENSISION_HOME}/logs
-  ln -s ${SENSISION_DATA_DIR}/metrics ${SENSISION_HOME}/metrics
-  ln -s ${SENSISION_DATA_DIR}/targets ${SENSISION_HOME}/targets
-  ln -s ${SENSISION_DATA_DIR}/queued ${SENSISION_HOME}/queued
+  ln -s "${SENSISION_DATA_DIR}/etc" "${SENSISION_HOME}/etc"
+  ln -s "${SENSISION_DATA_DIR}/scripts" "${SENSISION_HOME}/scripts"
+  ln -s "${SENSISION_DATA_DIR}/logs" "${SENSISION_HOME}/logs"
+  ln -s "${SENSISION_DATA_DIR}/metrics" "${SENSISION_HOME}/metrics"
+  ln -s "${SENSISION_DATA_DIR}/targets" "${SENSISION_HOME}/targets"
+  ln -s "${SENSISION_DATA_DIR}/queued" "${SENSISION_HOME}/queued"
 fi
 
 # Disable failing as the chown could fail, for example when hfiles contains volumes mounted read only
 set +e
-chown -Rf warp10:warp10 ${WARP10_DATA_DIR}
-chown -Rf sensision:sensision ${SENSISION_DATA_DIR}
+chown -Rf warp10:warp10 "${WARP10_DATA_DIR}"
+chown -Rf sensision:sensision "${SENSISION_DATA_DIR}"
